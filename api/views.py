30351636d93c3dfee085bf1b50dc46bd75f3dc8c
Anil Soni <anky.soni8460@gmail.com>
2019-11-19 23:25:04 +0530

    sort

--- api/views.py
+++ api/views.py
@@ -5,6 +5,36 @@ from . import app_usage
 from django.http import JsonResponse
 
 
+def sort(data):
+    points_to_remove = []
+    points = []
+    for idx, point in enumerate(data):
+        if isinstance(point['time'], str):
+            points_to_remove.append(idx)
+    points_to_remove = sorted(points_to_remove, reverse=True)
+
+    for idx in points_to_remove:
+        points.append(data.pop(idx))
+
+    return sorted(data, key=lambda x: x['time']) + points
+
+
+def to_hour(data):
+    data = sort(data)
+    data_with_hour = []
+
+    for point in data:
+        if not isinstance(point['time'], str):
+            hour = int(point['time'] // 60)
+            minute = int(point['time'] % 60)
+
+            data_with_hour.append({'name': point['name'], 'time': f'{hour}:{minute}'})
+        else:
+            data_with_hour.append(point)
+
+    return data_with_hour
+
+
 def predict(request, gender, week, app_name, user_id):
 
     db_table = {
@@ -26,4 +56,7 @@ def predict(request, gender, week, app_name, user_id):
     user = list(user.values_list('w_name', 'w_date', 'open_time', 'close_time', 'latitude',
                                  'longitude', 'location', 'gender'))
 
-    return JsonResponse(app_usage.main(user), safe=False)
+    data = app_usage.main(user)
+    data = to_hour(data)
+    return JsonResponse(data, safe=False)
+
