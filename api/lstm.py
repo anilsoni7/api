e1d6121714578b9647328de3456930cb0a3df58c
Anil Soni <anky.soni8460@gmail.com>
2020-03-19 22:54:55 +0530

    shape

--- api/lstm.py
+++ api/lstm.py
@@ -108,7 +108,10 @@ def main(data, split):
         name = d.name.iloc[0]
 
         start_in_minutes = preprocess(d.start)
+        if start_in_minutes.shape[0] == 1:
+            start_in_minutes.append(start_in_minutes.values.tolist() * 5)
         x, y = create_dataset(start_in_minutes.values)
+        print('dataset size', x.shape)
 
         if x.ndim < 1:
             print(f'didnt got data for {idx}')
