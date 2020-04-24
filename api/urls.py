bbf96da3406e6f6165c138ad0064da5a359303ec
Anil Soni <anky.soni8460@gmail.com>
2020-04-24 22:19:38 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import predict, lstm_view
 urlpatterns = [
     # gender : male|female,
     # week: week1 | week2
+    path('predict/<str:gender>/<str:week>/<str:app_name>/<int:user_id>/<int:split>', view=predict),
     path('predict/<str:gender>/<str:week>/<str:app_name>/<int:user_id>/<int:split>/<str:error>', view=predict),
     path('predict/<str:app_name>/<int:user_id>/<int:split>', view=lstm_view)
 ]
