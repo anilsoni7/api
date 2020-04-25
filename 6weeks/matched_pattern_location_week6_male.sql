68fdf617985aa9caa6dfc7f5ccd1c45201cecee2
Anil Soni <anky.soni8460@gmail.com>
2020-04-25 19:58:25 +0530

    Add files via upload

--- 6weeks/matched_pattern_location_week6_male.sql
+++ 6weeks/matched_pattern_location_week6_male.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 16, 2020 at 09:37 AM
+-- Generation Time: Apr 21, 2020 at 07:58 PM
 -- Server version: 5.6.41-84.1-log
 -- PHP Version: 7.2.7
 
